4d50dd6c5532b771e5acab14e7ad81045fb469ef
asadpro <asadjani202@gmail.com>
2022-06-19 09:34:05 +0500

    In this session i have completed most of the operations that we can do with numbers

---
+++ playing with numbers.py
@@ -0,0 +1,26 @@
+# python use 2 multiplication symbols to represent exponent.
+print(3 ** 3)
+decimal = 0.222347/0.3
+print(round(decimal,3)) # round the number upto 3 decimal point omit the rest of.
+
+print(type(4/2)) # By default python take every number as float until we decide it integer
+
+# 🍟🍟 Underscores in Numbers
+
+
+# Writing long number sometime confusing so we can put the underscore with every number
+# number = 936,20,4 if we write number like this it will act as tuple which we will cover latter
+number = 14_000_000_000
+print(number) # python will print this digit as numbers
+
+# 🍟🍟 Multiple Assignment
+fname , lnam = "Asad","Pro"
+print(fname,lnam)
+
+# 🍟🍟 Constants in python
+# Python doesn't have any constant datatype but programmers use all capital
+# letter to indicate a variable should be constant.
+PI = 3.14
+print(PI)
+
+    
